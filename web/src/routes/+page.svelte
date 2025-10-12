0274bcee0759a5e31ca6fa26168f50436aec82e1
hyperdefined <hyper@canine.tools>
2025-10-12 17:43:19 -0400

    remove popup message on homepage

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,41 +1,9 @@
 <script>
     import { t } from "$lib/i18n/translations";
-    import { onMount, tick } from "svelte";
-    import { get } from "svelte/store";
-    import dialogs, { createDialog } from "$lib/state/dialogs";
 
     import Omnibox from "$components/save/Omnibox.svelte";
     import Meowbalt from "$components/misc/Meowbalt.svelte";
     import SupportedServices from "$components/save/SupportedServices.svelte";
-
-    onMount(async () => {
-        await tick();
-        setTimeout(() => {
-            if (get(dialogs).length === 0) {
-                createDialog({
-                    id: "download-notice",
-                    type: "small",
-                    meowbalt: "smile",
-                    buttons: [
-                        {
-                            text: get(t)("button.gotit"),
-                            main: false,
-                            action: () => { },
-                        },
-                        {
-                            text: get(t)("button.cobalt_directory"),
-                            main: false,
-                            action: () => {
-                                window.open("https://cobalt.directory", "_blank", "noopener");
-                            }
-                        }
-                    ],
-                    bodyText:
-                        get(t)("general.main_status")
-                });
-            }
-        }, 0);
-    });
 </script>
 
 <svelte:head>
